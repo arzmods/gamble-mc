876d74d54775d6d98f20f7583135a433ff6b2789
arzmods <adminjrpcs@gmail.com>
2026-08-26 17:35:45 -0400

    Update imports for key mapping and networking

--- src/main/java/com/gamblemod/GambleModClient.java
+++ src/main/java/com/gamblemod/GambleModClient.java
@@ -3,8 +3,8 @@ package com.gamblemod;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.client.KeyMapping;
 
